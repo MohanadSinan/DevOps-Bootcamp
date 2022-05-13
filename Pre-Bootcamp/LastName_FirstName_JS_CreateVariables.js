c00f2f80a663daf4f1566cedd9560cb08892bab3
Mohanad Sinan <mohanad14141@hotmail.com>
2022-05-13 03:36:21 +0300

    first commit

---
+++ Pre-Bootcamp/LastName_FirstName_JS_CreateVariables.js
@@ -0,0 +1,9 @@
+// The rider must be at least 42 inches tall and over the age of 10
+
+var conditionalTall = 42;   // The rider must be at least 42 inches tall
+var conditionalAge  = 10;   // The rider must be over the age of 10
+
+var riderTall;  // The rider's height [number]
+var riderAge;   // The rider's Age [number]
+
+var isTheRiderApproved; // Check whether the rider is tall enough and old enough to ride the rollercoaster [Boolean]
