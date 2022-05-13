723bd03f31caca70a8834616aff717416887ff7d
Mohanad Sinan <mohanad14141@hotmail.com>
2022-05-14 02:00:42 +0300

    Complete a new challenge & organize files

---
+++ Pre-Bootcamp/[Code Challenge] Get On That Ride/MeasuresRider'sHeight.js
@@ -0,0 +1,8 @@
+var conditionalHeight = 52;
+var riderHeight;
+
+if (riderHeight > conditionalHeight) {
+    console.log("Get on that ride, kiddo!");
+} else {
+    console.log("Sorry kiddo. Maybe next year.");
+}
---
+++ Pre-Bootcamp/[Code Challenge] Get On That Ride/MeasuresRider'sHeightOrAge.js
@@ -0,0 +1,11 @@
+var conditionalHeight = 52;
+var conditionalAge = 10;
+
+var riderHeight;
+var riderAge;
+
+if (riderHeight > conditionalHeight || riderAge > conditionalAge) {
+    console.log("Get on that ride, kiddo!");
+} else {
+    console.log("Sorry kiddo. Maybe next year.");
+}
