5c84cb41f1723fba74a5503d296458eac15c9713
Mohanad Sinan <mohanad14141@hotmail.com>
2022-05-15 09:39:29 +0300

    Complete a new challenge

---
+++ Pre-Bootcamp/[Code Challenge] Debug/debugTheCode.js
@@ -0,0 +1,6 @@
+function greeting(){
+    return "Hello World";
+}
+var word = greeting();
+// console.log();          // [Error] console.log() method does NOT return data!
+console.log(word);         // [Fix] return the variable word.
